46327074041979ae3beadaba8b06e57c6f6d5e10
Aditya <adityas.singh.tech@gmail.com>
2025-07-21 03:09:34 +0530

    feat: configure router in the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import AppRouter from './Routes/AppRouter.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={AppRouter}>
     <App />
+    </RouterProvider>
   </StrictMode>,
 )
