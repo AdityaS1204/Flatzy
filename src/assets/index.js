de2aab6f5a160c8ad090713973e21f719cf70ec8
Aditya <adityas.singh.tech@gmail.com>
2025-07-21 03:04:34 +0530

    add images for about and grid section

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import ownerImg from "./generated-Image.png"
+import gridImg1 from "./grid1.png"
+import gridImg2 from "./grid2.png"
+import gridImg3 from "./grid3.png"
+export {ownerImg, gridImg1, gridImg2, gridImg3}
