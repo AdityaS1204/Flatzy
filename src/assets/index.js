e0ed12b3c54ce3fa01efb72b689adaa360120bdd
Aditya <adityas.singh.tech@gmail.com>
2025-07-22 22:46:04 +0530

    image import

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,4 +2,5 @@ import ownerImg from "./generated-Image.png"
 import gridImg1 from "./grid1.png"
 import gridImg2 from "./grid2.png"
 import gridImg3 from "./grid3.png"
-export {ownerImg, gridImg1, gridImg2, gridImg3}
+import gridImg4 from "./grid4.png"
+export {ownerImg, gridImg1, gridImg2, gridImg3, gridImg4}
