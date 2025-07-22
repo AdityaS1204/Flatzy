90fb5aa63b42a7c5952ab80c2e0a8f62c7365a96
Aditya <adityas.singh.tech@gmail.com>
2025-07-22 22:08:59 +0530

    components exported

---
+++ src/Components/index.js
@@ -0,0 +1,33 @@
+import AdminLogin from "./AdminLogin";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import HomeCard from "./HomeCard";
+import Services from "./Services";
+import About from "./About";
+import ReferNEarn from "./ReferNEarn";
+import Footer from "./Footer";
+import Testimonial from "./Testimonial";
+import FAQ from "./FAQ";
+import PropertyForm from "./PropertyForm";
+import PropertyList from "./PropertyList";
+import PropertyCard from "./PropertyCard";
+import DashboardHeader from "./DashboardHeader";
+import DashboardLayout from "./DashboardLayout";
+
+export {
+  AdminLogin, 
+  Navbar, 
+  Hero,
+  HomeCard,
+  Services,
+  About,
+  ReferNEarn,
+  Footer,
+  Testimonial,
+  FAQ,
+  PropertyForm,
+  PropertyList,
+  PropertyCard,
+  DashboardHeader,
+  DashboardLayout
+}
