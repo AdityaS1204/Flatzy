2530621e1278a63e012e330ba277ebaec7292721
Aditya <adityas.singh.tech@gmail.com>
2025-07-22 22:43:03 +0530

    feat: add msg arguments

--- src/Utils/Whatsapp.js
+++ src/Utils/Whatsapp.js
@@ -1,6 +1,6 @@
-export const whatsappRedirect = (msg) => {
+export const whatsappRedirect = (message) => {
     const phone = "+918767798079"
-    const msg = msg
+    const msg = message
     const encodedUrlComponent = encodeURIComponent(msg);
     const url = `https://wa.me/${phone}?text=${encodedUrlComponent}`
     window.open(url, "_blank");
