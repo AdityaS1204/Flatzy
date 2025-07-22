3cf4db566d73db5aa0e1dae0d2682f951fdd27fe
Aditya <adityas.singh.tech@gmail.com>
2025-07-22 22:36:49 +0530

    feat: made the component reuseable

--- src/Components/Marquee.jsx
+++ src/Components/Marquee.jsx
@@ -7,7 +7,7 @@ const MarqueeComponent = ({direction,speed,cardSize,imgSrc}) => {
        <Marquee pauseOnHover gradient gradientColor="" gradientWidth={0} loop={0} speed={speed} autoFill direction={direction} className="flex mask-r-from-96 mask-l-from-80 flex-col">
 {imgSrc.map((img,index)=>(
     <div className='my-0 p-1'>
-        <img src={img} alt="students in pg" className={`${cardSize ? cardSize:'h-28 w-34'} rounded-2xl object-cover`} />
+        <img src={img} alt="students in pg" className={`${cardSize ? cardSize:'h-28 w-34'} border-2 border-gray-300 rounded-2xl object-cover`} />
     </div>
 ))}
 </Marquee>
