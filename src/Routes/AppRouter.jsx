a694d5bdd6bb696e4c1185578209254145c01c09
Aditya <adityas.singh.tech@gmail.com>
2025-07-23 00:40:53 +0530

    route fix

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,26 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
-import { Admin, Home, Listings } from "../Pages";
+import { Admin,Home,Listings } from "../Pages";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "fz-admin",
-        element: <Admin />,
-      },
-      {
-        path: "listings",
-        element: <Listings />,
-      }
-    ]
-  }
-]);
 
-export default router;
+
+const AppRouter = createBrowserRouter([
+    {
+        path:'',
+        element:<App/>,
+        children:[
+            {
+                path:"/",
+                element:<Home/>,
+            },
+            {
+                path:"/fz-admin",
+                element:<Admin/>,
+            },
+            {
+                path:"/listings",
+                element:<Listings/>,
+            }
+        ]
+    }
+])
+
+export default AppRouter
