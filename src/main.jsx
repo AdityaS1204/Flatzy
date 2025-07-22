c9ae9a8bc8edbba3a2eb5f8f9e29b25a41db302c
Aditya <adityas.singh.tech@gmail.com>
2025-07-22 23:45:20 +0530

    Fix Vercel configuration and React Router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import {AppRouter} from './Routes/AppRouter.jsx'
+import AppRouter from './Routes/AppRouter.jsx'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
