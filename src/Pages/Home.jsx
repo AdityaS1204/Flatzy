e08cbff09fc629efe4cc08901cfd1c11a6d3682d
Aditya <adityas.singh.tech@gmail.com>
2025-07-22 22:38:37 +0530

    feat: add whatsapp chat button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,11 @@ import {motion} from 'motion/react'
 import { Navbar, Hero, HomeCard, Services, About, ReferNEarn, Footer,Testimonial, FAQ } from '../Components'
 import Marquee from "react-fast-marquee";
 import { whatsappRedirect } from '../Utils/Whatsapp';
-
+import { Link } from 'react-router-dom';
 const Home = () => {
 
-const msg = "hey, there i want to know more about the services you are offering"
-
+const chatmsg = "hey, there i want to know more about the services you are offering"
+const listmsg = "hey, there i am [your name],\n i want to list my property:\n These are the details of the property:\n [property details]"
   return (
     <div className="min-h-screen font-sans bg-[#FADA7A]">
       <Navbar />
@@ -15,10 +15,10 @@ const msg = "hey, there i want to know more about the services you are offering"
       </section>
       <div className="flex justify-center w-full gap-4 mt-8">
         <div className='flex justify-between w-6/12'>
-          <button className="bg-black text-white ring-2 ring-white px-6 py-3 rounded-full hover:scale-105 hover:cursor-pointer duration-300 transition">
+          <Link to="/listings"><button className="bg-black text-white ring-2 ring-white px-6 py-3 rounded-full hover:scale-105 hover:cursor-pointer duration-300 transition">
             Find Your Stay
-          </button>
-          <button className="border  text-black ring-2 ring-black px-6 py-3 rounded-full hover:bg-gray-100 transition duration-300">
+          </button></Link>
+          <button className="border  text-black ring-2 ring-black px-6 py-3 rounded-full hover:bg-gray-100 transition duration-300" onClick={()=> whatsappRedirect(listmsg)}>
             List Your Property
           </button>
         </div>
@@ -56,7 +56,7 @@ const msg = "hey, there i want to know more about the services you are offering"
       whileInView={{opacity:1,y:0}}
       transition={{duration:0.5,ease:'easeOut'}}
       viewport={{once:true,amount:0.3}}
-       className='fixed bottom-10 right-10 bg-green-600 z-30 text-black p-4 rounded-full shadow-lg' onClick={whatsappRedirect(msg)}>Chat On Whatsapp</motion.div>
+       className='fixed bottom-10 right-10 bg-green-600 z-30 text-black p-4 rounded-full shadow-lg' onClick={()=>whatsappRedirect(chatmsg)}>Chat On Whatsapp</motion.div>
     </div>
   )
 }
