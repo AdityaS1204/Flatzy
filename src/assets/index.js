69beb74aba94a6b69ca7c970165974c1fbfd456d
Aditya <adityas.singh.tech@gmail.com>
2025-07-25 21:51:15 +0530

    new image imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,12 @@ import gridImg1 from "./grid1.png"
 import gridImg2 from "./grid2.png"
 import gridImg3 from "./grid3.png"
 import gridImg4 from "./grid4.png"
-export {ownerImg, gridImg1, gridImg2, gridImg3, gridImg4}
+import gridImg5 from "./grid5.png"
+import gridImg6 from "./grid6.png"
+import heroImg1 from "./heroimg1.png"
+import heroImg2 from "./heroimg2.jpg"
+import heroImg3 from "./heroimg3.jpg"
+
+
+
+export {ownerImg, gridImg1, gridImg2, gridImg3, gridImg4,heroImg1,heroImg2,heroImg3,gridImg5,gridImg6}
